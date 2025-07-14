5022fa64ec63a73b9b629bd0b3edc0cd7eda3cc0
Eugene600 <ugowach160@gmail.com>
2025-07-14 17:33:04 +0300

    feat: Included background icons in first section of home page

--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -2,28 +2,20 @@ import { Component } from '@angular/core';
 import { ServicesSection } from "../../components/services-section/services-section";
 import { TechSection } from "../../components/tech-section/tech-section";
 import { Footer } from '../../../../core/layout/footer/footer';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faDatabase, faMobileAlt, faComments, faChurch, faChampagneGlasses, faMoneyBillWave } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [ServicesSection, TechSection , Footer],
+  imports: [ServicesSection, TechSection, Footer, FontAwesomeModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-  //to look at later
-  ngAfterViewInit() {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          (entry.target as HTMLElement).classList.add('visible');
-        }
-      });
-    }, { threshold: 0.2 });
-
-    document.querySelectorAll('.page-section').forEach(section => {
-      observer.observe(section);
-    });
-  }
-
-
+  faDatabase = faDatabase;
+  faMobile = faMobileAlt;
+  faSocial = faComments;
+  faChapel = faChurch;
+  faEvent = faChampagneGlasses;
+  faPayment = faMoneyBillWave;
 }
