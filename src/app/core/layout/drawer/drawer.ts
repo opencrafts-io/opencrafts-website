2ff13d11e68f57be405aae343dac9387607f51f8
Eugene600 <ugowach160@gmail.com>
2025-07-15 14:56:27 +0300

    feat: Added animations for opening drawer - Have to work on animation for closing drawer as well

--- src/app/core/layout/drawer/drawer.ts
+++ src/app/core/layout/drawer/drawer.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-drawer',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './drawer.html',
   styleUrl: './drawer.css'
 })
 export class Drawer {
+   @Output() linkClicked = new EventEmitter<void>();
 
+  handleLinkClick() {
+    this.linkClicked.emit();
+  }
 }
